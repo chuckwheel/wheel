d8bf5173af2eb5f6d1b3b1b9613573103e4a618f
chuckhuang <eyes1842@163.com>
2016-05-14 15:19:15 +0800

    a

--- wheel/src/main/java/io/wheel/ErrorCode.java
+++ wheel/src/main/java/io/wheel/ErrorCode.java
@@ -9,15 +9,15 @@ package io.wheel;
  */
 public interface ErrorCode {
 
-	String SUCCCESS = "SYS-000";
-	String FAILURE = "SYS-999";
+	String SUCCCESS = "RPC-000";
+	String FAILURE = "RPC-999";
 
-	String NO_PROTOCOL = "SYS-001";// 协议未定义
-	String NO_SERVICE = "SYS-002";// 服务未定义
-	String NO_PROVIDER = "SYS-003";// 服务未定义
+	String NO_PROTOCOL = "RPC-001";// 协议未定义
+	String NO_SERVICE = "RPC-002";// 服务未定义
+	String NO_PROVIDER = "RPC-003";// 服务未定义
 
-	String SERVICE_INVOKE_ERROR = "SYS-004";
-	String SERVICE_TIMEOUT = "SYS-005";
-	String SERVICE_INTERRUPTED = "SYS-006";
+	String SERVICE_INVOKE_ERROR = "RPC-004";
+	String SERVICE_TIMEOUT = "RPC-005";
+	String SERVICE_INTERRUPTED = "RPC-006";
 
 }
